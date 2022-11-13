2f008da82287a0b91091b44f341c01d114568754
Ryunosky <Ryunosky@DESKTOP-7B745P4>
2022-11-13 12:49:44 +0900

    プロジェクト ファイルを追加します。

---
+++ Multi Explorer/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Multi_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.explorerBrowser1 = new Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser();
+            this.explorerBrowser2 = new Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser();
+            this.explorerBrowser3 = new Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser();
+            this.explorerBrowser4 = new Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(9, 9);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer1.Size = new System.Drawing.Size(1246, 662);
+            this.splitContainer1.SplitterDistance = 322;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.DoubleClick += new System.EventHandler(this.splitContainer1_DoubleClick);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.explorerBrowser1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.explorerBrowser2);
+            this.splitContainer2.Size = new System.Drawing.Size(1246, 322);
+            this.splitContainer2.SplitterDistance = 621;
+            this.splitContainer2.TabIndex = 0;
+            this.splitContainer2.DoubleClick += new System.EventHandler(this.splitContainer2_DoubleClick);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.explorerBrowser3);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.explorerBrowser4);
+            this.splitContainer3.Size = new System.Drawing.Size(1246, 336);
+            this.splitContainer3.SplitterDistance = 621;
+            this.splitContainer3.TabIndex = 0;
+            this.splitContainer3.DoubleClick += new System.EventHandler(this.splitContainer3_DoubleClick);
+            // 
+            // explorerBrowser1
+            // 
+            this.explorerBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.explorerBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.explorerBrowser1.Name = "explorerBrowser1";
+            this.explorerBrowser1.PropertyBagName = "Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser";
+            this.explorerBrowser1.Size = new System.Drawing.Size(619, 320);
+            this.explorerBrowser1.TabIndex = 0;
+            // 
+            // explorerBrowser2
+            // 
+            this.explorerBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.explorerBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.explorerBrowser2.Name = "explorerBrowser2";
+            this.explorerBrowser2.PropertyBagName = "Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser";
+            this.explorerBrowser2.Size = new System.Drawing.Size(619, 320);
+            this.explorerBrowser2.TabIndex = 0;
+            // 
+            // explorerBrowser3
+            // 
+            this.explorerBrowser3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.explorerBrowser3.Location = new System.Drawing.Point(0, 0);
+            this.explorerBrowser3.Name = "explorerBrowser3";
+            this.explorerBrowser3.PropertyBagName = "Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser";
+            this.explorerBrowser3.Size = new System.Drawing.Size(619, 334);
+            this.explorerBrowser3.TabIndex = 0;
+            // 
+            // explorerBrowser4
+            // 
+            this.explorerBrowser4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.explorerBrowser4.Location = new System.Drawing.Point(0, 0);
+            this.explorerBrowser4.Name = "explorerBrowser4";
+            this.explorerBrowser4.PropertyBagName = "Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser";
+            this.explorerBrowser4.Size = new System.Drawing.Size(619, 334);
+            this.explorerBrowser4.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 671);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 10);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            this.panel1.DoubleClick += new System.EventHandler(this.panel1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Multi Explorer";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser explorerBrowser1;
+        private Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser explorerBrowser2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser explorerBrowser3;
+        private Microsoft.WindowsAPICodePack.Controls.WindowsForms.ExplorerBrowser explorerBrowser4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Multi Explorer/Form1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.WindowsAPICodePack.Controls;
+using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_Explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            explorerBrowser1.NavigationOptions.PaneVisibility.Commands = PaneVisibilityState.Hide;
+            explorerBrowser2.NavigationOptions.PaneVisibility.Commands = PaneVisibilityState.Hide;
+            explorerBrowser3.NavigationOptions.PaneVisibility.Commands = PaneVisibilityState.Hide;
+            explorerBrowser4.NavigationOptions.PaneVisibility.Commands = PaneVisibilityState.Hide;
+
+            explorerBrowser1.Navigate((ShellObject)KnownFolders.Documents);
+            explorerBrowser2.Navigate((ShellObject)KnownFolders.Documents);
+            explorerBrowser3.Navigate((ShellObject)KnownFolders.Documents);
+            explorerBrowser4.Navigate((ShellObject)KnownFolders.Documents);
+        }
+
+        private void panel1_DoubleClick(object sender, EventArgs e)
+        {
+            splitContainer1.Panel2Collapsed = false;
+            panel1.Visible = false;
+        }
+
+        private void splitContainer1_DoubleClick(object sender, EventArgs e)
+        {
+            splitContainer1.Panel2Collapsed = true;
+            panel1.Visible = true;
+        }
+
+        private void splitContainer2_DoubleClick(object sender, EventArgs e)
+        {
+            splitContainer2.SplitterDistance = (splitContainer2.Width - 4) / 2;
+        }
+
+        private void splitContainer3_DoubleClick(object sender, EventArgs e)
+        {
+            splitContainer3.SplitterDistance = (splitContainer3.Width - 4) / 2;
+        }
+    }
+}
